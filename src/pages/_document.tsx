d04ef3aa384fa7183d2e68e4c8f2e0bff4894cdb
mongj <evan.zhangmingjun@gmail.com>
2023-05-02 11:41:31 +0800

    build: add chakra-ui

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+
+import ChakraProviders from '../providers/chakra';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head />
+            <body>
+                <ChakraProviders>
+                    <Main />
+                    <NextScript />
+                </ChakraProviders>
+            </body>
+        </Html>
+    );
 }
